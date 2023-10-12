e5e35988de3c99a8a4b5172336fb7a07b2fbbc33
Topsurpass <temitopeabiodun685@gmail.com>
2023-10-12 21:18:28 +0000

    resolved bugs

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,12 +1,6 @@
--- Write a SQL script that lists all bands with Glam rock as their main
--- style, ranked by their longevity
--- The expression IFNULL(split, 2020) - IFNULL(formed, 0) is used to ensure
--- that if the "split" column is null, it's treated as if it contains the
--- value 2020, and if the "formed" column is null, it's treated as if it
--- contains the value 0. This ensures we do not have a null value for lifespan
--- calculation.
-
-
-SELECT band_name, IFNULL(split, 2020) - IFNULL(formed, 0)
-AS lifespan FROM metal_bands WHERE style like '%Glam rock%'
-ORDER BY lifespan DESC;
+-- Lists all bands with Glam rock as their main style, ranked by their longevity.
+-- SELECT band_name, (IFNULL(split, YEAR(CURRENT_DATE())) - formed) AS lifespan
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
