31f239d0d78bc04549290cd8a740f1336e29eec1
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2023-10-19 22:30:53 +0000

    testing differences

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,43 +1,32 @@
 #!/usr/bin/env python3
-"""This module contains example of how cache functionality
-in web browsers works"""
+""" Redis Module """
 
-import redis
-from typing import Callable
 from functools import wraps
+import redis
 import requests
+from typing import Callable
 
-"""Establish connection to the database using default details"""
-redisdb = redis.Redis()
+redis_ = redis.Redis()
 
 
-def wrap_get_page(fxn: Callable) -> Callable:
-    """A decorated function definition for the cache"""
-
-    @wraps(fxn)
-    def wrapper(url):
-        """A wrapper function for the get_page function"""
-
-        """Track how many times a particular URL was accessed in the
-        key count:{url} by creating key and automatically setting &
-        increasing it's value by 1 per access"""
-        redisdb.incr(f"count:{url}")
-
-        """Get cached data, if found, decode and return it as str,
-        else set cached key and fix its value as get_page() return
-        value with 10 secs expiration time"""
-        cached_response = redisdb.get(f"cached:{url}")
-        if cached_response:
-            return cached_response.decode('utf-8')
-        newVal = fxn(url)
-        redisdb.setex(f"cached:{url}", 10, newVal)
-        return newVal
+def count_requests(method: Callable) -> Callable:
+    """ Decortator for counting """
+    @wraps(method)
+    def wrapper(url):  # sourcery skip: use-named-expression
+        """ Wrapper for decorator """
+        redis_.incr(f"count:{url}")
+        cached_html = redis_.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
+        html = method(url)
+        redis_.setex(f"cached:{url}", 10, html)
+        return html
 
     return wrapper
 
 
-@wrap_get_page
+@count_requests
 def get_page(url: str) -> str:
-    """A function that get the body of a particular url"""
+    """ Obtain the HTML content of a  URL """
     req = requests.get(url)
     return req.text
